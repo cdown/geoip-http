ad3b968edce8515b96926e437d8df61e66371259
Chris Down <chris@chrisdown.name>
2023-05-23 16:19:40 +0100

    reload: Add online DB reloading support

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,13 @@ use axum::{Json, Router};
 use axum_client_ip::InsecureClientIp;
 use clap::Parser;
 use maxminddb::{Mmap, Reader};
+use once_cell::sync::OnceCell;
 use serde::Serialize;
 use std::net::{IpAddr, SocketAddr};
 use std::sync::Arc;
+use std::time::{Duration, Instant};
 use tokio::signal;
+use tokio::sync::{Mutex, RwLock};
 
 #[derive(Parser, Debug)]
 struct Config {
@@ -23,6 +26,14 @@ struct Config {
     /// The location of the GeoLite2 database
     #[arg(short, long, default_value = "GeoLite2-City.mmdb")]
     db: String,
+
+    /// Minimum time before db reloads at /reload_geoip
+    #[arg(long, default_value = "60")]
+    db_reload_secs: u64,
+
+    /// Disable DB reloading at runtime
+    #[arg(long)]
+    disable_db_reloading: bool,
 }
 
 #[derive(Serialize)]
@@ -37,10 +48,11 @@ struct TimezoneErrorResponse {
     ip: String,
 }
 
-fn get_tz(
-    reader: Arc<Reader<Mmap>>,
+async fn get_tz(
+    reader: Arc<RwLock<Reader<Mmap>>>,
     ip: IpAddr,
 ) -> Result<Json<TimezoneResponse>, Json<TimezoneErrorResponse>> {
+    let reader = reader.read().await;
     match reader.lookup::<maxminddb::geoip2::City>(ip) {
         Ok(city) => Ok(Json(TimezoneResponse {
             tz: city
@@ -57,17 +69,46 @@ fn get_tz(
 
 async fn get_tz_with_client_ip(
     InsecureClientIp(insecure_client_ip): InsecureClientIp,
-    Extension(reader): Extension<Arc<Reader<Mmap>>>,
+    Extension(reader): Extension<Arc<RwLock<Reader<Mmap>>>>,
 ) -> impl IntoResponse {
     // We use the insecure one to get X-Forwarded-For, etc
-    get_tz(reader, insecure_client_ip)
+    get_tz(reader, insecure_client_ip).await
 }
 
 async fn get_tz_with_explicit_ip(
-    Extension(reader): Extension<Arc<Reader<Mmap>>>,
+    Extension(reader): Extension<Arc<RwLock<Reader<Mmap>>>>,
     Path(ip): Path<IpAddr>,
 ) -> impl IntoResponse {
-    get_tz(reader, ip)
+    get_tz(reader, ip).await
+}
+
+async fn reload_geoip(
+    Extension(reader): Extension<Arc<RwLock<Reader<Mmap>>>>,
+    Extension(cfg): Extension<Arc<Config>>,
+) -> String {
+    static NEXT_RELOAD_TIME: OnceCell<Mutex<Instant>> = OnceCell::new();
+
+    if cfg.disable_db_reloading {
+        return "reloading disabled".to_string();
+    }
+
+    let now = Instant::now();
+    let reload_time = NEXT_RELOAD_TIME.get_or_init(|| Mutex::new(now));
+
+    let mut reload_time = reload_time.lock().await;
+    if *reload_time <= now {
+        match Reader::open_mmap(&cfg.db) {
+            Ok(new_reader) => {
+                let mut old_reader = reader.write().await;
+                *old_reader = new_reader;
+                *reload_time = now + Duration::from_secs(cfg.db_reload_secs);
+                "reloaded".to_string()
+            }
+            Err(err) => err.to_string(),
+        }
+    } else {
+        "too early to reload".to_string()
+    }
 }
 
 async fn wait_for_shutdown_request() {
@@ -82,12 +123,14 @@ async fn wait_for_shutdown_request() {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let cfg = Config::parse();
-    let reader = Arc::new(Reader::open_mmap(cfg.db)?);
+    let cfg = Arc::new(Config::parse());
+    let reader = Arc::new(RwLock::new(Reader::open_mmap(&cfg.db)?));
     let app = Router::new()
         .route("/", get(get_tz_with_client_ip))
+        .route("/reload_geoip", get(reload_geoip))
         .route("/:ip", get(get_tz_with_explicit_ip))
-        .layer(Extension(reader));
+        .layer(Extension(reader))
+        .layer(Extension(cfg.clone()));
     let addr = SocketAddr::from((cfg.ip, cfg.port));
     Ok(axum::Server::bind(&addr)
         .serve(app.into_make_service_with_connect_info::<SocketAddr>())
