c4ab014c8c024fa8fcccb58d4e6eab2c854d9382
Chris Down <chris@chrisdown.name>
2023-05-23 04:52:46 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,118 @@
+use axum::{
+    extract::{Extension, Path},
+    response::IntoResponse,
+    routing::get,
+    Json, Router,
+};
+use axum_client_ip::InsecureClientIp;
+use clap::Parser;
+use maxminddb::Reader;
+use serde::Serialize;
+use std::{
+    net::{IpAddr, SocketAddr},
+    sync::Arc,
+};
+use tokio::signal;
+
+#[derive(Parser, Debug)]
+struct Config {
+    /// The IP to listen on
+    #[arg(short, long, default_value = "0.0.0.0")]
+    ip: IpAddr,
+
+    /// The port to listen on
+    #[arg(short, long, default_value = "3000")]
+    port: u16,
+
+    /// The location of the GeoLite2 database
+    #[arg(short, long, default_value = "GeoLite2-City.mmdb")]
+    db: String,
+}
+
+#[derive(Serialize)]
+struct TimezoneResponse {
+    tz: Option<String>,
+    ip: String,
+}
+
+#[derive(Serialize)]
+struct TimezoneErrorResponse {
+    error: String,
+    ip: String,
+}
+
+fn get_tz(
+    reader: Arc<Reader<Vec<u8>>>,
+    ip: IpAddr,
+) -> Result<Json<TimezoneResponse>, Json<TimezoneErrorResponse>> {
+    let city: Result<maxminddb::geoip2::City, _> = reader.lookup(ip);
+    let city = match city {
+        Ok(city) => city,
+        Err(err) => {
+            return Err(Json(TimezoneErrorResponse {
+                ip: ip.to_string(),
+                error: err.to_string(),
+            }))
+        }
+    };
+    Ok(Json(TimezoneResponse {
+        tz: city
+            .location
+            .as_ref()
+            .and_then(|loc| loc.time_zone.map(str::to_string)),
+        ip: ip.to_string(),
+    }))
+}
+
+async fn get_tz_with_client_ip(
+    InsecureClientIp(insecure_client_ip): InsecureClientIp,
+    Extension(reader): Extension<Arc<Reader<Vec<u8>>>>,
+) -> impl IntoResponse {
+    // We use the insecure one to get X-Forwarded-For, etc
+    get_tz(reader, insecure_client_ip)
+}
+
+async fn get_tz_with_explicit_ip(
+    Extension(reader): Extension<Arc<Reader<Vec<u8>>>>,
+    Path(ip): Path<IpAddr>,
+) -> impl IntoResponse {
+    get_tz(reader, ip)
+}
+
+async fn signal_shutdown() {
+    let ctrl_c = async { signal::ctrl_c().await.unwrap() };
+
+    #[cfg(unix)]
+    let unix_sig = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let unix_sig = std::future::pending::<()>(); // unimplemented elsewhere
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = unix_sig => {},
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let cfg = Config::parse();
+
+    let reader = Arc::new(Reader::open_readfile(cfg.db)?);
+    let app = Router::new()
+        .route("/", get(get_tz_with_client_ip))
+        .route("/:ip", get(get_tz_with_explicit_ip))
+        .layer(Extension(reader));
+
+    let addr = SocketAddr::from((cfg.ip, cfg.port));
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service_with_connect_info::<SocketAddr>())
+        .with_graceful_shutdown(signal_shutdown())
+        .await?;
+    unreachable!()
+}
